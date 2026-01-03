1dc364d4c76afd32b0528e6df8bf0e790c925a7e
izen67 <17254913+izen67@users.noreply.github.com>
2026-01-03 16:12:37 +0200

    Simplify hyprland Home Manager imports

--- common/desktop/hyprland/default.nix
+++ common/desktop/hyprland/default.nix
@@ -6,15 +6,9 @@
         ./hyprland.nix
     ];
 
-
 # Attach hyprland Home Manager configs only when this desktop is active
   home-manager.users.${user}.imports = [
-    # Main hyprland config and theme
-    ../../home-manager/hyprland/hyprland.nix
-    ../../home-manager/hyprland/theme.nix
-
-    # Choose what dotfiles to use (custom or noctalia)
-    #../../home-manager/hyprland/custom/default.nix
-    ../../home-manager/hyprland/noctalia/default.nix
+    # Main hm hyprland import
+    ../../home-manager/hyprland/default.nix
   ];
 }
