6e5c50f961c6664af7b168328a1bc16a6cca2c85
izen67 <17254913+izen67@users.noreply.github.com>
2026-01-10 17:34:45 +0200

    Update noctalia.nix

--- common/home-manager/hyprland/noctalia/noctalia.nix
+++ common/home-manager/hyprland/noctalia/noctalia.nix
@@ -11,30 +11,30 @@
     enable = true;
     systemd.enable = true;
 
- #   plugins = {
- #     sources = [
- #       {
- #         enabled = true;
- #         name = "Official Noctalia Plugins";
- #         url = "https://github.com/noctalia-dev/noctalia-plugins";
- #       }
- #     ];
- #     states = {
- #       privacy-indicator = {
- #         enabled = true;
- #         sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
- #       };
- #     };
- #     version = 1;
- #   };
-#
-#    pluginSettings = {
-#      privacy-indicator = {
-#        hideInactive = false;
-#        iconSpacing = "4";
-#        removeMargins = false;
-#      };
-#    };
+    plugins = {
+      sources = [
+        {
+          enabled = true;
+          name = "Official Noctalia Plugins";
+          url = "https://github.com/noctalia-dev/noctalia-plugins";
+        }
+      ];
+      states = {
+        privacy-indicator = {
+          enabled = true;
+          sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+        };
+      };
+      version = 1;
+    };
+
+    pluginSettings = {
+      privacy-indicator = {
+        hideInactive = false;
+        iconSpacing = "4";
+        removeMargins = false;
+      };
+    };
 
     settings = {
       appLauncher = {
