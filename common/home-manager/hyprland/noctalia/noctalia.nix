4ee85a7e76beee9c64b7f28eba353963020b672b
izen67 <17254913+izen67@users.noreply.github.com>
2026-01-10 17:25:06 +0200

    Update noctalia.nix

--- common/home-manager/hyprland/noctalia/noctalia.nix
+++ common/home-manager/hyprland/noctalia/noctalia.nix
@@ -11,23 +11,23 @@
     enable = true;
     systemd.enable = true;
 
-    plugins = {
-      sources = [
-        {
-          enabled = true;
-          name = "Official Noctalia Plugins";
-          url = "https://github.com/noctalia-dev/noctalia-plugins";
-        }
-      ];
-      states = {
-        privacy-indicator = {
-          enabled = true;
-          sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
-        };
-      };
-      version = 1;
-    };
-
+ #   plugins = {
+ #     sources = [
+ #       {
+ #         enabled = true;
+ #         name = "Official Noctalia Plugins";
+ #         url = "https://github.com/noctalia-dev/noctalia-plugins";
+ #       }
+ #     ];
+ #     states = {
+ #       privacy-indicator = {
+ #         enabled = true;
+ #         sourceUrl = "https://github.com/noctalia-dev/noctalia-plugins";
+ #       };
+ #     };
+ #     version = 1;
+ #   };
+#
 #    pluginSettings = {
 #      privacy-indicator = {
 #        hideInactive = false;
